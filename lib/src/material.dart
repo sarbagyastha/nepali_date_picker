8892ea12606d8fd7d90a598f15e1665e6bb9688f
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2025-03-27 10:59:24 +0545

    bumped version

--- lib/src/material.dart
+++ lib/src/material.dart
@@ -10,9 +10,9 @@ import 'nepali_calendar_delegate.dart';
 /// user confirms the dialog. If the user cancels the dialog, null is returned.
 Future<NepaliDateTime?> showNepaliDatePicker({
   required BuildContext context,
-  NepaliDateTime? initialDate,
   required NepaliDateTime firstDate,
   required NepaliDateTime lastDate,
+  NepaliDateTime? initialDate,
   NepaliDateTime? currentDate,
   DatePickerEntryMode initialEntryMode = DatePickerEntryMode.calendar,
   SelectableDayPredicate? selectableDayPredicate,
@@ -38,83 +38,39 @@ Future<NepaliDateTime?> showNepaliDatePicker({
   final Icon? switchToInputEntryModeIcon,
   final Icon? switchToCalendarEntryModeIcon,
 }) async {
-  const calendarDelegate = NepaliCalendarDelegate();
-  initialDate =
-      initialDate == null ? null : calendarDelegate.dateOnly(initialDate);
-  firstDate = calendarDelegate.dateOnly(firstDate);
-  lastDate = calendarDelegate.dateOnly(lastDate);
-  assert(
-    !lastDate.isBefore(firstDate),
-    'lastDate $lastDate must be on or after firstDate $firstDate.',
-  );
-  assert(
-    initialDate == null || !initialDate.isBefore(firstDate),
-    'initialDate $initialDate must be on or after firstDate $firstDate.',
-  );
-  assert(
-    initialDate == null || !initialDate.isAfter(lastDate),
-    'initialDate $initialDate must be on or before lastDate $lastDate.',
-  );
-  assert(
-    selectableDayPredicate == null ||
-        initialDate == null ||
-        selectableDayPredicate(initialDate),
-    'Provided initialDate $initialDate must satisfy provided selectableDayPredicate.',
-  );
-  assert(debugCheckHasMaterialLocalizations(context));
-
-  Widget dialog = DatePickerDialog(
-    calendarDelegate: calendarDelegate,
-    initialDate: initialDate,
+  final date = await showDatePicker(
+    context: context,
     firstDate: firstDate,
     lastDate: lastDate,
+    initialDate: initialDate,
     currentDate: currentDate,
     initialEntryMode: initialEntryMode,
     selectableDayPredicate: selectableDayPredicate,
     helpText: helpText,
     cancelText: cancelText,
     confirmText: confirmText,
-    initialCalendarMode: initialDatePickerMode,
-    errorFormatText: errorFormatText,
-    errorInvalidText: errorInvalidText,
-    fieldHintText: fieldHintText,
-    fieldLabelText: fieldLabelText,
-    keyboardType: keyboardType,
-    onDatePickerModeChange: onDatePickerModeChange,
-    switchToInputEntryModeIcon: switchToInputEntryModeIcon,
-    switchToCalendarEntryModeIcon: switchToCalendarEntryModeIcon,
-  );
-
-  if (textDirection != null) {
-    dialog = Directionality(textDirection: textDirection, child: dialog);
-  }
-
-  if (locale != null) {
-    dialog =
-        Localizations.override(context: context, locale: locale, child: dialog);
-  } else {
-    final datePickerTheme = DatePickerTheme.of(context);
-    if (datePickerTheme.locale != null) {
-      dialog = Localizations.override(
-        context: context,
-        locale: datePickerTheme.locale,
-        child: dialog,
-      );
-    }
-  }
-
-  return showDialog<NepaliDateTime>(
-    context: context,
+    locale: locale,
     barrierDismissible: barrierDismissible,
     barrierColor: barrierColor,
     barrierLabel: barrierLabel,
     useRootNavigator: useRootNavigator,
     routeSettings: routeSettings,
-    builder: (BuildContext context) {
-      return builder == null ? dialog : builder(context, dialog);
-    },
+    textDirection: textDirection,
+    builder: builder,
+    initialDatePickerMode: initialDatePickerMode,
+    errorFormatText: errorFormatText,
+    errorInvalidText: errorInvalidText,
+    fieldHintText: fieldHintText,
+    fieldLabelText: fieldLabelText,
+    keyboardType: keyboardType,
     anchorPoint: anchorPoint,
+    onDatePickerModeChange: onDatePickerModeChange,
+    switchToInputEntryModeIcon: switchToInputEntryModeIcon,
+    switchToCalendarEntryModeIcon: switchToCalendarEntryModeIcon,
+    calendarDelegate: const NepaliCalendarDelegate(),
   );
+
+  return date as NepaliDateTime?;
 }
 
 /// Shows a full screen modal dialog containing a Material Design date range
@@ -125,9 +81,9 @@ Future<NepaliDateTime?> showNepaliDatePicker({
 /// returned.
 Future<DateTimeRange<NepaliDateTime>?> showNepaliDateRangePicker({
   required BuildContext context,
-  DateTimeRange<NepaliDateTime>? initialDateRange,
   required NepaliDateTime firstDate,
   required NepaliDateTime lastDate,
+  DateTimeRange<NepaliDateTime>? initialDateRange,
   NepaliDateTime? currentDate,
   DatePickerEntryMode initialEntryMode = DatePickerEntryMode.calendar,
   String? helpText,
@@ -155,60 +111,12 @@ Future<DateTimeRange<NepaliDateTime>?> showNepaliDateRangePicker({
   final Icon? switchToCalendarEntryModeIcon,
   SelectableDayForRangePredicate? selectableDayPredicate,
 }) async {
-  const calendarDelegate = NepaliCalendarDelegate();
-  initialDateRange = initialDateRange == null
-      ? null
-      : calendarDelegate.datesOnly(initialDateRange);
-  firstDate = calendarDelegate.dateOnly(firstDate);
-  lastDate = calendarDelegate.dateOnly(lastDate);
-  assert(
-    !lastDate.isBefore(firstDate),
-    'lastDate $lastDate must be on or after firstDate $firstDate.',
-  );
-  assert(
-    initialDateRange == null || !initialDateRange.start.isBefore(firstDate),
-    "initialDateRange's start date must be on or after firstDate $firstDate.",
-  );
-  assert(
-    initialDateRange == null || !initialDateRange.end.isBefore(firstDate),
-    "initialDateRange's end date must be on or after firstDate $firstDate.",
-  );
-  assert(
-    initialDateRange == null || !initialDateRange.start.isAfter(lastDate),
-    "initialDateRange's start date must be on or before lastDate $lastDate.",
-  );
-  assert(
-    initialDateRange == null || !initialDateRange.end.isAfter(lastDate),
-    "initialDateRange's end date must be on or before lastDate $lastDate.",
-  );
-  assert(
-    initialDateRange == null ||
-        selectableDayPredicate == null ||
-        selectableDayPredicate(
-          initialDateRange.start,
-          initialDateRange.start,
-          initialDateRange.end,
-        ),
-    "initialDateRange's start date must be selectable.",
-  );
-  assert(
-    initialDateRange == null ||
-        selectableDayPredicate == null ||
-        selectableDayPredicate(
-            initialDateRange.end, initialDateRange.start, initialDateRange.end),
-    "initialDateRange's end date must be selectable.",
-  );
-  currentDate =
-      calendarDelegate.dateOnly(currentDate ?? calendarDelegate.now());
-  assert(debugCheckHasMaterialLocalizations(context));
-
-  Widget dialog = DateRangePickerDialog(
-    calendarDelegate: calendarDelegate,
-    initialDateRange: initialDateRange,
+  final dateRange = await showDateRangePicker(
+    context: context,
     firstDate: firstDate,
     lastDate: lastDate,
+    initialDateRange: initialDateRange,
     currentDate: currentDate,
-    selectableDayPredicate: selectableDayPredicate,
     initialEntryMode: initialEntryMode,
     helpText: helpText,
     cancelText: cancelText,
@@ -221,38 +129,21 @@ Future<DateTimeRange<NepaliDateTime>?> showNepaliDateRangePicker({
     fieldEndHintText: fieldEndHintText,
     fieldStartLabelText: fieldStartLabelText,
     fieldEndLabelText: fieldEndLabelText,
-    keyboardType: keyboardType,
-    switchToInputEntryModeIcon: switchToInputEntryModeIcon,
-    switchToCalendarEntryModeIcon: switchToCalendarEntryModeIcon,
-  );
-
-  if (textDirection != null) {
-    dialog = Directionality(textDirection: textDirection, child: dialog);
-  }
-
-  if (locale != null) {
-    dialog =
-        Localizations.override(context: context, locale: locale, child: dialog);
-  }
-
-  final range = await showDialog<DateTimeRange>(
-    context: context,
+    locale: locale,
     barrierDismissible: barrierDismissible,
     barrierColor: barrierColor,
     barrierLabel: barrierLabel,
     useRootNavigator: useRootNavigator,
     routeSettings: routeSettings,
-    useSafeArea: false,
-    builder: (BuildContext context) {
-      return builder == null ? dialog : builder(context, dialog);
-    },
+    textDirection: textDirection,
+    builder: builder,
     anchorPoint: anchorPoint,
+    keyboardType: keyboardType,
+    switchToInputEntryModeIcon: switchToInputEntryModeIcon,
+    switchToCalendarEntryModeIcon: switchToCalendarEntryModeIcon,
+    selectableDayPredicate: selectableDayPredicate,
+    calendarDelegate: const NepaliCalendarDelegate(),
   );
 
-  if (range == null) return null;
-
-  return DateTimeRange(
-    start: NepaliDateTime(range.start.year, range.start.month, range.start.day),
-    end: NepaliDateTime(range.end.year, range.end.month, range.end.day),
-  );
+  return dateRange as DateTimeRange<NepaliDateTime>?;
 }
